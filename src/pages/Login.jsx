8fa61c2aca58f0984a997ed86b9681c59a1790d8
Uttam Kumar Barman <uttam.web2025@gmail.com>
2026-03-30 20:30:37 +0600

    add login page and register page with toggle fitcher

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Login = () => {
+    const handleLogin = (e) => {
+        e.preventDefault();
+        const form= e.target;
+        const email= form.email.value;
+        const password= form.password.value;
+        console.log(email, password);
+    }
     return (
-        <div>
-            login
+        <div className="card bg-base-100 mx-auto my-20 w-full max-w-sm shrink-0 shadow-2xl">
+            <div className="card-body">
+                <h1 className="text-4xl font-bold">Login now!</h1>
+                <form onSubmit={handleLogin} className="fieldset">
+                    <label className="label">Email</label>
+                    <input type="email" className="input" name='email' placeholder="Email" />
+                    <label className="label">Password</label>
+                    <input type="password" className="input" name='password' placeholder="Password" />
+                    <div><a className="link link-hover">Forgot password?</a></div>
+                    <button type='submit' className="btn btn-neutral mt-4">Login</button>
+                </form>
+                <p>Don't have an account ?Please <Link to='/register' className='text-blue-500 font-bold'>Register</Link></p>
+            </div>
         </div>
     );
 };
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,18 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Register = () => {
+    const handleRegister = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const formData = new FormData(form);
+        console.log(formData);
+
+        const { email, password, ...restFormData } = Object.fromEntries(formData.entries());
+    }
     return (
-        <div className="card bg-base-100 mx-auto w-full max-w-sm shrink-0 shadow-2xl">
+        <div className="card bg-base-100 mx-auto my-10 w-full max-w-sm shrink-0 shadow-2xl">
             <div className="card-body">
-                <h1 className="text-5xl font-bold">Register now!</h1>
-                <fieldset className="fieldset">
+                <h1 className="text-4xl font-bold">Register now!</h1>
+                <form onSubmit={handleRegister} className="fieldset">
+                    <label className='label'>Name</label>
+                    <input className='input' type="text" name="name" placeholder='User Name' />
+                    <label className='label'>Photo URL</label>
+                    <input className='input' type="text" name="photo" placeholder='Photo URL' />
                     <label className="label">Email</label>
-                    <input type="email" className="input" placeholder="Email" />
+                    <input type="email" className="input" name='email' placeholder="Email" />
                     <label className="label">Password</label>
-                    <input type="password" className="input" placeholder="Password" />
+                    <input type="password" className="input" name='password' placeholder="Password" />
                     <div><a className="link link-hover">Forgot password?</a></div>
-                    <button className="btn btn-neutral mt-4">Login</button>
-                </fieldset>
+                    <button type='submit' className="btn btn-neutral mt-4">Register</button>
+                </form>
+                <p>Already have an account ?Please <Link to='/login' className='text-blue-500 font-bold'>Login</Link></p>
             </div>
         </div>
     );
